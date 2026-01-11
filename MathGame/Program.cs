d83d2ae50948ca8dc6cb8d93f110e0a244756aca
Keith Edwards <89081584+kedwards1306@users.noreply.github.com>
2026-01-11 17:20:42 -0500

    Adding more to userChoices method and creation of Question method

--- MathGame/Program.cs
+++ MathGame/Program.cs
@@ -5,10 +5,6 @@ int substract = 0;
 int multiply = 0;
 int division = 0;
 
-
-
-
-
 Menu();
 void Menu() {
     Console.WriteLine("------------------------------------");
@@ -16,6 +12,7 @@ void Menu() {
     Console.WriteLine("What is your name?");
     string userName = Console.ReadLine();
     Console.WriteLine($"Hello {userName}, let's start the game!");
+    Console.WriteLine("Please choose your choices");
 
 }
 
@@ -29,6 +26,32 @@ string UserChoices()
 
     string choice = Console.ReadLine();
     return choice;
+    
+    if (choice.Trim() == "1")
+    {
+        Console.WriteLine(" Addition Selected");
+    }
+    else if (choice.Trim() == "2")
+    {
+        Console.WriteLine(" Subtraction Selected");
+    }
+    else if (choice.Trim() == "3")
+    {
+        Console.WriteLine(" Multiplication Selected");
+    }
+    else if (choice.Trim() == "4")
+    {
+        Console.WriteLine(" Division Selected");
+    }
+    else if (choice.Trim().ToLower() == "q")
+    {
+        Console.WriteLine("Goodbye");
+        Environment.Exit(0);
+    }
+    else
+    {
+        Console.WriteLine(" Invalid Choice");
+    }
 }
 
 //creating Choices in switch break cases
@@ -37,4 +60,38 @@ string UserChoices()
 // selection 3. - Multiplication so userInput1 * userInput2
 // selection 4. - Division so userInput1 / userInput2
 // selection 5. - could be random or more than one formula
-//
+
+void Questions(string questions)
+{
+    Random random = new Random();
+    int num1 = random.Next(1, 10);
+    int num2 = random.Next(1, 10);
+    int result;
+    switch (questions)
+    {
+        case "1":
+            Console.WriteLine($"What is {num1} + {num2}? ");
+            result = num1 + num2;
+            break;
+        case "2":
+            Console.WriteLine($"What is {num1} - {num2}? ");
+            result = num1 - num2;
+            break;
+        case "3":
+            Console.WriteLine($"What is {num1} x {num2}? ");
+            result = num1 * num2;
+            break;
+        case "4":
+        result = num1 / num2;
+            if (num2 != 0)
+        {
+             Console.WriteLine($"What is {num1} / {num2}? ");
+        }
+            
+            break;
+        default:
+            Console.WriteLine("Invalid choice");
+            break;
+    }
+    
+}
